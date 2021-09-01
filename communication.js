217137d78517e6ad87729fa189d8b66a56b8f76f
Raul Vasile <raul.vasile@everymatrix.com>
2021-09-01 12:15:47 +0300

    change all options to only replaysubject

--- communication.js
+++ communication.js
@@ -7,46 +7,17 @@ let subjects = {};
  * @name topic
  * @description A small wrapper over the rxjs to offer the topic method on top of it
  * @param event [String] Event name
- * @param type [String] Type of the subject - Possible values: Subject | BehaviorSubject | ReplaySubject
- * @param step [String] How many values to be buffed for new subscribers
- * @returns Subject | BehaviorSubject | ReplaySubject
+ * @param step [String] How many values to be buffed for new subscribers - default 0
+ * @returns ReplaySubject
  */
 
-const topic = (event, type, step) => {
-  this.event = event;
-
+const topic = (event, step = 0) => {
   if (topics.indexOf(event) == -1) {
+    let replaySubject = new ReplaySubject(step);
+
+    subjects[event] = replaySubject;
+    
     topics.push(event);
-
-    switch(type) {
-      case 'Subject':
-        let subject = new Subject();
-
-        subjects[event] = subject;
-
-        break;
-
-      case 'BehaviorSubject':
-        let behaviorSubject = new BehaviorSubject(0);
-
-        subjects[event] = behaviorSubject;
-
-        break;
-
-      case 'ReplaySubject':
-        let replaySubject = new ReplaySubject(step);
-
-        subjects[event] = replaySubject;
-
-        break;
-
-      default:
-        let def = new Subject();
-
-        subjects[event] = def;
-
-        break;
-    }
   }
 
   return subjects[event];
